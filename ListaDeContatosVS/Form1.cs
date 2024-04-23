a7a3cf27e79c2a511e1ce6b3bde7c798aeefcba8
ErickBanhos99 <erick.bcastro99@gmail.com>
2024-04-23 13:53:42 -0300

    Continuação 1 Lista de ctt
    Algumas coisas a mais hoje, 23/04/24

--- ListaDeContatosVS/Form1.cs
+++ ListaDeContatosVS/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListaDeContatosVS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,22 @@ using System.Windows.Forms;
 
 namespace ListaDeContatosVS
 {
-    public partial class Form1 : Form
+     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonIncluirContato_Click(object sender, EventArgs e)
+        {
+            // Cria um objeto da classe contato.
+            Contatos contato = new Contatos();
+            contato.Nome = textBoxNome.Text;
+            contato.Sobrenome = textBoxSobrenome.Text;
+            contato.Telefone = textBoxTelefone.Text;
+
+            listBoxContatos.Items.Add(contato.ToString());
         }
     }
 }
